d6558edc9c23b75ab2acb6ae92adc661685d7f4d
geckoHex <123674650+geckoHex@users.noreply.github.com>
2025-12-04 16:51:52 -0800

    added skeleton for Plant class

---
+++ Plant.java
@@ -0,0 +1,18 @@
+public class Plant {
+    protected double price;
+    protected boolean flowers;
+    protected String name;
+
+    public Plant(String name, double price, boolean flowers) {
+
+    }
+
+    @Override
+    public String toString() {
+        
+    }
+
+    public void printBill(int plantsToOrder) {
+
+    }
+}
