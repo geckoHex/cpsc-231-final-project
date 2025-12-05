4932a0154ce696bb3ede466412767416aa9cfa5a
geckoHex <123674650+geckoHex@users.noreply.github.com>
2025-12-04 17:11:31 -0800

    implement most of printBill; left rounding as a to-do

--- Plant.java
+++ Plant.java
@@ -1,3 +1,6 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class Plant {
     protected double price;
     protected boolean flowers;
@@ -19,6 +22,42 @@ public class Plant {
     }
 
     public void printBill(int plantsToOrder) {
+        try {
+            PrintWriter outputFileWriter = new PrintWriter("receipt.txt");
+
+            // Header
+            outputFileWriter.println("**** Receipt from The Plant Store ****\n");
+
+            // Print items ordered
+            for (int i=0; i<plantsToOrder; i++) {
+                outputFileWriter.println(i+1 + ") " + name + " -------- $" + price); // Print each item and price for the amount ordered, indexed starting at 1
+            }
+            outputFileWriter.println(); // Blank line
+
+            // Calculate totals
+            double subTotal = price * plantsToOrder; // TODO: Round to two digits
+            double taxCost = subTotal * 0.0775; // 7.75% tax rate in Orange, CA
+            double finalPrice = subTotal + taxCost; // TODO: Round to two digits
+
+            // Add finals to the receipt
+            outputFileWriter.println(); // New line
+            outputFileWriter.println("Sub-total: $" + subTotal);
+            outputFileWriter.println("Tax:       $" + taxCost);
+            outputFileWriter.println("----");
+            outputFileWriter.println("Final total: $" + finalPrice);
+            outputFileWriter.println();
+
+            // Add cute message
+            outputFileWriter.println("Thank you, come again!");
+
+
+            // Save and close the file
+            outputFileWriter.close();
+        } catch (FileNotFoundException e) {
+            // This code is unreachable because the JVM will create the file if it
+            // doesn't exists. However, the compiler forces us to handle this error,
+            // so this try-catch is merely a mandated formality here.
+        }
 
     }
 }
