ef38e3f2193938adf104cd2485fd9169b88d6609
geckoHex <123674650+geckoHex@users.noreply.github.com>
2025-12-04 16:52:20 -0800

    implement plant constructor

--- Plant.java
+++ Plant.java
@@ -4,12 +4,14 @@ public class Plant {
     protected String name;
 
     public Plant(String name, double price, boolean flowers) {
-
+        this.name = name;
+        this.price = price;
+        this.flowers = flowers;
     }
 
     @Override
     public String toString() {
-        
+
     }
 
     public void printBill(int plantsToOrder) {
